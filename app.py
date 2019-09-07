3ef867f897e816793298ad453ce29a5a7753b697
freakie123 <ralphmensah123@gmail.com>
2019-09-07 20:43:23 +0000

    initial commit

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("getweather.html")
+    return render_template("index.html")
 
 if __name__=='__main__':
-    app.run(debug=True, host='localhost')
+    app.run(debug=True, host='127.0.0.1')
