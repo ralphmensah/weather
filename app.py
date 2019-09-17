7376b3b5f8398e774c03222b825a9e6d2e418c15
freakie123 <ralphmensah123@gmail.com>
2019-09-17 01:00:08 +0000

    new commit

--- app.py
+++ app.py
@@ -1,13 +1,35 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+from pprint import pprint
 import requests
-
+from datetime import datetime
+from datetime import timedelta
 app = Flask(__name__)
 
-
-
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    
+    try:
+        url='http://api.openweathermap.org/data/2.5/weather?q={}&APPID=9a9e6eeb16ffef5a4c5c1ba9bf0bffc4'
+    # city = "Accra"
+    
+        data=[]
+        if request.method == 'POST':
+            city = str(request.form.get('city').capitalize())
+    
+            request_url = (requests.get(url.format(city))).json()
+        
+            weather = {
+                "city": city,
+                "temperature":request_url["main"]["temp"],
+                "discription":request_url["weather"][0]["description"],
+                "icon": request_url["weather"][0]["icon"]
+                    }
+            data.append(weather)
+    except:
+        print('hello')
+    return render_template("index.html",  data = data)
+
+
 
 if __name__=='__main__':
-    app.run(debug=True, host='127.0.0.1')
+    app.run(debug=True)
