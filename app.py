83ece69634d5029881ec2aed7acbfcca0da589de
freakie123 <ralphmensah123@gmail.com>
2019-10-01 23:53:05 +0000

    server error fixed

--- app.py
+++ app.py
@@ -5,32 +5,39 @@ app = Flask(__name__)
 
 # app.config['SECRET_KEY'] = '16e6f9980e1feee2ebe8dad714089a3589fd42ab'
 
+@app.route("/home", methods=['GET', 'POST'])
 @app.route("/", methods=['GET', 'POST'])
 def index():
     # form = NewForm()
-    
-    url='http://api.openweathermap.org/data/2.5/weather?q={}&APPID=9a9e6eeb16ffef5a4c5c1ba9bf0bffc4&units=metric'
+    try:
+        url='http://api.openweathermap.org/data/2.5/weather?q={}&APPID=9a9e6eeb16ffef5a4c5c1ba9bf0bffc4&units=metric'
     # city = "Accra"
-    data=[]
-    if request.method == 'POST':
-        city = str(request.form.get('city').capitalize())
-        request_url = (requests.get(url.format(city))).json()
-        weather = {
-            "city": city,
-            "temperature":round(request_url["main"]["temp"]),
-            "discription":request_url["weather"][0]["description"],
-            "icon": request_url["weather"][0]["icon"]
-                }
-        data.append(weather)
+        data=[]
+        if request.method == 'POST':
+            city = str(request.form.get('city').capitalize())
+            request_url = (requests.get(url.format(city))).json()
+            weather = {
+                "city": city,
+                "temperature":round(request_url["main"]["temp"]),
+                "discription":request_url["weather"][0]["description"],
+                "icon": request_url["weather"][0]["icon"]
+                    }
+            data.append(weather)
     
-        
+    except:
+        pass
+
     return render_template("index.html",  data = data)
-
-
+    
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404page.html", title="404 page")
+
+
+@app.errorhandler(500) 
+def server_failure(e):
+    render_template("<h1>server failure</h1>") 
 
 
 if __name__=='__main__':
