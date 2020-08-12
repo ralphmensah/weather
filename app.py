a71394f55576a088654c7dec3bc785b8a374f714
ralphmensah <ralphmensah123@gmail.com>
2020-08-12 01:49:37 +0000

    Fixed:Input Box to Reqiured

--- app.py
+++ app.py
@@ -10,12 +10,13 @@ app.config['SECRET_KEY'] = '16e6f9980e1feee2ebe8dad714089a3589fd42ab'
 def index():
     # form = NewForm()
     try:
-        url='http://api.openweathermap.org/data/2.5/weather?q={}&APPID=9a9e6eeb16ffef5a4c5c1ba9bf0bffc4&units=metric'
+
         # city = "Accra"
         data=[]
         if request.method == 'POST':
             city = str(request.form.get('city').capitalize())
-            request_url = (requests.get(url.format(city))).json()
+            url=f'http://api.openweathermap.org/data/2.5/weather?q={city}&APPID=9a9e6eeb16ffef5a4c5c1ba9bf0bffc4&units=metric'
+            request_url = (requests.get(city)).json()
             weather = {
                 "city": city,
                 "temperature":round(request_url["main"]["temp"]),
@@ -23,23 +24,23 @@ def index():
                 "icon": request_url["weather"][0]["icon"]
                     }
             data.append(weather)
-    
+
     except:
-        
+
         flash(f'Weather Information not Found for {city.title()}')
         return redirect(url_for('index'))
 
     return render_template("index.html",  data = data)
-    
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404page.html", title="404 page")
 
 
-@app.errorhandler(500) 
+@app.errorhandler(500)
 def server_failure(e):
-    render_template("<h1>server failure</h1>") 
+    render_template("<h1>server failure</h1>")
 
 
 if __name__=='__main__':
