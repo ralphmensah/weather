5f4b6e6a2479d780cc3ad519db6d1804832b8b2e
Ralph Mensah <ralphmensah123@gmail.com>
2020-08-28 11:48:57 +0000

    Delete form.py

--- form.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
-from wtforms.validators import DataRequired
-
-class NewForm(FlaskForm):
-    location = StringField(validators=[DataRequired()])
-    submit = SubmitField("Search")
